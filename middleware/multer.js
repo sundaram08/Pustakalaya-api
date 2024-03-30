cc5f1fea928cc57375a738305b0587aa362d276e
sundaram08 <sundaramdwivedi2003@gmail.com>
2024-03-30 23:40:40 +0530

    fixing path

--- middleware/multer.js
+++ middleware/multer.js
@@ -1,7 +1,8 @@
 const multer = require('multer')
+const a = require('')
 const storage = multer.diskStorage({
   destination: function (req, file ,cb){
-    cb(null,"public/temp");
+    cb(null,"../public/temp");
   },
   filename:function(req,file,cb){
     cb(null, file.originalname)
