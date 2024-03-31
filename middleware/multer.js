68190f70ebb12092cb4253b054ff903569893f61
sundaram08 <sundaramdwivedi2003@gmail.com>
2024-03-31 11:24:22 +0530

    /tmp

--- middleware/multer.js
+++ middleware/multer.js
@@ -1,7 +1,8 @@
 const multer = require('multer')
 const path = require('path');
 
-const uploadPath = path.join(__dirname, '..', 'public', 'temp');
+// const uploadPath = path.join(__dirname, '..', 'public', 'temp');   EROFS error
+const uploadPath = '/tmp';
 const storage = multer.diskStorage({
   destination: function (req, file ,cb){
     cb(null,uploadPath);
